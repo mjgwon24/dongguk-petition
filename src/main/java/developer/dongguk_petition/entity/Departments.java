32aeb868f3b3aa5a7bc48afa14b76e758a9eb606
minji <alswlchlrh8@naver.com>
2024-10-18 20:20:01 +0900

    jpa 관련 파일구조 업데이트

--- src/main/java/developer/dongguk_petition/entity/Departments.java
+++ src/main/java/developer/dongguk_petition/entity/Departments.java
@@ -1,11 +1,21 @@
 package developer.dongguk_petition.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 부서 정보 엔티티
  * @since
  * @latest
  * @author 김진수
  */
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Departments {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long id;
 }
--- src/main/java/developer/dongguk_petition/entity/Petitions.java
+++ src/main/java/developer/dongguk_petition/entity/Petitions.java
@@ -1,11 +1,21 @@
 package developer.dongguk_petition.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 청원 정보 엔티티
  * @since
  * @latest
  * @author 김진수
  */
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Petitions {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "petition_id")
+    private Long id;
 }
